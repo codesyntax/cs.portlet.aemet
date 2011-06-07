0e70384f42679eaec417246ee341959aca233668
dreguera <drbakhache@gmail.com>
2011-06-07 12:42:41 +0000

    initial commit
    svn path=/cs.portlet.aemet/trunk/; revision=46954

---
+++ cs/portlet/aemet/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+AEMETPortletMessageFactory = MessageFactory('cs.portlet.aemet')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
